fb50990ca679b4b0df8b3316940293b551abfa08
Rory Campbell <rorycampbell@Rorys-MacBook-Air.local>
2019-10-25 14:11:31 +1000

    Updates

---
+++ precache-manifest.d0dcf9ba5cb23c526ce12b4f77708ee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153c191728ee30db23b162cb5e90672c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9ce20a9c23c20e4f76dc",
+    "url": "/portfolio/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6986237449fb7e95459d",
+    "url": "/portfolio/static/css/main.c5c20fef.chunk.css"
+  },
+  {
+    "revision": "9ce20a9c23c20e4f76dc",
+    "url": "/portfolio/static/js/2.c6e81278.chunk.js"
+  },
+  {
+    "revision": "6986237449fb7e95459d",
+    "url": "/portfolio/static/js/main.8e7f65a4.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  }
+]);
