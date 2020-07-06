d0b5de01bf9c23f140e5cb38aff9c2e2a05bc874
Rory Campbell <rorycampbell@Rorys-MacBook-Air-2.local>
2020-07-06 21:13:44 +1000

    Updates

---
+++ precache-manifest.1e7e03025a68e5ba3a528547cfe919b7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d97925f0d20d7120144dfdfd95fdc1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4204df48d3929d1bf73d",
+    "url": "/portfolio/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "40d62e83da6e54705dcc",
+    "url": "/portfolio/static/css/main.1a30926c.chunk.css"
+  },
+  {
+    "revision": "4204df48d3929d1bf73d",
+    "url": "/portfolio/static/js/2.cd0f4738.chunk.js"
+  },
+  {
+    "revision": "40d62e83da6e54705dcc",
+    "url": "/portfolio/static/js/main.c2c1d5d5.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  },
+  {
+    "revision": "bd6fd13c54b8600d32100fc920df5a67",
+    "url": "/portfolio/static/media/Class-planner.bd6fd13c.jpeg"
+  },
+  {
+    "revision": "56bd29defb0622d0d7625a6b8ed9dbac",
+    "url": "/portfolio/static/media/Dave-site.56bd29de.png"
+  },
+  {
+    "revision": "22ed3a27f3f0539d87167c879e03fa1c",
+    "url": "/portfolio/static/media/Deer-jess.22ed3a27.png"
+  },
+  {
+    "revision": "f7353c2074da53d47c55202040000df6",
+    "url": "/portfolio/static/media/Rory-story.f7353c20.png"
+  },
+  {
+    "revision": "7908c50696965cad10f1d2abc0962d9f",
+    "url": "/portfolio/static/media/Smile.7908c506.png"
+  },
+  {
+    "revision": "9d69a0f9b6592c82b72e506f0d4a0ae5",
+    "url": "/portfolio/static/media/Yelp-camp.9d69a0f9.png"
+  }
+]);
